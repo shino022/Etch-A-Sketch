34d74a5377ed6b799dfe70061ac46d790452346a
sss <sshoony95@gmail.com>
2022-06-17 11:44:08 -0400

    Fix createGrid() to remove existing grids

--- index.js
+++ index.js
@@ -1,20 +1,25 @@
 const container = document.querySelector('.container');
-const numOfGrid = 16;
-for(let i = 0; i < numOfGrid; i++) {
-  //create row
-  const row = document.createElement('div');
-  row.classList.add('row');
-  for(let j = 0; j < numOfGrid; j++) {
-    //create a square and append it into the row
-    const square = document.createElement('div');
-    square.classList.add('square');
-    row.appendChild(square);
+const DefaultNumOfGrid = 16;
+function createGrid(numOfGrid){
+  //remove all contents
+  while(container.firstChild) {
+    container.removeChild(container.firstChild);
   }
-  //append row into container
-  container.appendChild(row);
+  for(let i = 0; i < numOfGrid; i++) {
+    //create row
+    const row = document.createElement('div');
+    row.classList.add('row');
+    for(let j = 0; j < numOfGrid; j++) {
+      //create a square and append it into the row
+      const square = document.createElement('div');
+      square.classList.add('square');
+      row.appendChild(square);
+    }
+    //append row into container
+    container.appendChild(row);
+  }
 }
-
 function changeNumGrid() {
-  let numOfGridInput = prompt("Enter the number of squares per side");
+  const userNumOfGrid = prompt("Enter the number of squares per side");
 
 }
