9d4bfc595a321710ebbe39b28e6f20b14faeda03
sss <sshoony95@gmail.com>
2022-06-17 11:09:59 -0400

    Create rows

--- index.js
+++ index.js
@@ -1,1 +1,12 @@
 const container = document.querySelector('.container');
+const numOfGrid = 16;
+for(let i = 0; i < numOfGrid; i++) {
+  //create row
+  const row = document.createElement('div');
+  row.classList.add('row');
+  for(let j = 0; j < numOfGrid; j++) {
+    //create a square and append it into the row
+  }
+  //append row into container
+  container.appendChild(row);
+}
